8524df5079ff55b643d29e7be94b3c87d7c98174
Dmitrii Kovalenko <dmitrii.kovalenko.51@gmail.com>
2026-09-09 15:31:00 +0200

    fix(firebase): update firebase_options.dart with real Android credentials to prevent crash on startup

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,7 +1,16 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatform;
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      return const FirebaseOptions(
+        apiKey: 'AIzaSyB1QpyvBN157VrsORYErLOVqz2roWIfGY8',
+        appId: '1:646660581815:android:895cc9d8ca75b8172be032',
+        messagingSenderId: '646660581815',
+        projectId: 'hegemony-game',
+      );
+    }
     return const FirebaseOptions(
       apiKey: 'dummy-api-key',
       appId: '1:1234567890:ios:dummyapp',
